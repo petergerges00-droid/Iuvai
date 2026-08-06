7da1950f6e1bad316cf0c3a94976cc6057bee3d7
petergerges00-droid <peter.gerges00@gmail.com>
2026-08-07 01:22:12 +0300

    Create iuvai-logo.tsx

---
+++ artifacts/iuvai/src/components/iuvai/iuvai-logo.tsx
@@ -0,0 +1,12 @@
+import { Link } from "wouter";
+
+export function IuvaiLogo() {
+  return (
+    <Link
+      href="/"
+      className="text-lg font-semibold tracking-[0.28em] text-white transition-opacity hover:opacity-80"
+    >
+      IUVAI
+    </Link>
+  );
+}
